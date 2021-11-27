5a9f8d5863b43d9d982def6b69a6f9750e3c6865
Lucas Mota <lucasmota7@gmail.com>
2021-11-27 13:06:00 -0300

    Add project files.

---
+++ 14688 - Lucas Mota - Exercicio12ifelse.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio12IfElse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Informe o Nome do Usuario: ");
+                string nome = Console.ReadLine().ToUpper();
+
+                Console.Write("Informe a pontuação: ");
+                double pontuacao = double.Parse(Console.ReadLine());
+
+                if (pontuacao >= 0)
+                {
+                    Console.WriteLine("Informe a situação do usuario: " +
+                                        "\n(P) Escolaridade Publica;" +
+                                        "\n(A) Afrodescendente;" +
+                                        "\n(D) Deficiente;" +
+                                        "\n(N) Não se enquadra em nenhuma das situações acima.");
+                    string situacao = Console.ReadLine().ToUpper();
+
+                    if (situacao == "P")
+                    {
+                        pontuacao += pontuacao * 0.15;
+
+                        Console.WriteLine("Nome do Candidato: " + nome +
+                                            "\nPontuação Final do Candidato: " + pontuacao.ToString("F2"));
+                    }
+                    else if (situacao == "A")
+                    {
+                        pontuacao += pontuacao * 0.10;
+
+                        Console.WriteLine("Nome do Candidato: " + nome +
+                                            "\nPontuação Final do Candidato: " + pontuacao.ToString("F2"));
+                    }
+                    else if (situacao == "D")
+                    {
+                        pontuacao += pontuacao * 0.05;
+
+                        Console.WriteLine("Nome do Candidato: " + nome +
+                                            "\nPontuação Final do Candidato: " + pontuacao.ToString("F2"));
+                    }
+                    else if (situacao == "N")
+                    {
+                        Console.WriteLine("Nome do Candidato: " + nome +
+                                            "\nPontuação Final do Candidato: " + pontuacao.ToString("F2"));
+                    }
+                    else
+                        {
+                        Console.WriteLine("Opção Inválida.");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Insira um valor maior do que 0 (Zero)");
+                }
+            }
+            catch(System.FormatException)
+            {
+                Console.WriteLine("Insira apenas numeros.");
+            }
+            finally
+            {
+                Console.WriteLine("Programa encerrado.");
+                Console.ReadKey();                
+            }
+        }
+    }
+}
